67ac14b74cf0f2891509a7ef9250b7beda1a245e
miyaz <sinji_mg@yahoo.co.jp>
2021-04-18 19:38:17 +0900

    request handling sample2

--- samples/reqhandle/main.go
+++ samples/reqhandle/main.go
@@ -8,60 +8,151 @@ import (
 	"net/http"
 	"os"
 	"regexp"
-	"sort"
+	"strings"
 	"sync"
 )
 
 // DataStore ... Variables that use mutex
 type DataStore struct {
-	*sync.RWMutex
-	server    ServerInfo
+	host      HostInfo
+	resource  ResourceInfo
 	validator map[string]*regexp.Regexp
 }
 
-// ServerInfo ... information of server
-type ServerInfo struct {
-	Name string
-	IP   string
-	AZ   string
+// HostInfo ... information of host
+type HostInfo struct {
+	Name string `json:"name"`
+	IP   string `json:"ip"`
+	AZ   string `json:"az,omitempty"`
+}
+
+// ResourceInfo ... information of os resource
+type ResourceInfo struct {
+	CPU    ResourceUsage `json:"cpu"`
+	Memory ResourceUsage `json:"memory"`
+}
+
+// ResourceUsage  .. information of os resource usage
+type ResourceUsage struct {
+	*sync.RWMutex
+	Target  float64 `json:"target"`
+	Current float64 `json:"current"`
+}
+
+func (ru *ResourceUsage) getTarget() float64 {
+	ru.RLock()
+	defer ru.RUnlock()
+	return ru.Target
+}
+func (ru *ResourceUsage) setTarget(value float64) {
+	ru.Lock()
+	defer ru.Unlock()
+	ru.Target = value
+}
+func (ru *ResourceUsage) getCurrent() float64 {
+	ru.RLock()
+	defer ru.RUnlock()
+	return ru.Current
+}
+func (ru *ResourceUsage) setCurrent(value float64) {
+	ru.Lock()
+	defer ru.Unlock()
+	ru.Current = value
+}
+
+// RequestInfo ... information of request
+type RequestInfo struct {
+	Path      string            `json:"path"`
+	Query     string            `json:"querystring"`
+	Headers   map[string]string `json:"headers"`
+	Rawquery  *QueryString      `json:"rawquery"`
+	Evaluated *QueryString      `json:"validatedQuery"`
+}
+
+// ResponseInfo ... information of response
+type ResponseInfo struct {
+	Host     HostInfo     `json:"host"`
+	Resource ResourceInfo `json:"resource"`
+	Request  RequestInfo  `json:"request"`
+}
+
+var store = &DataStore{
+	HostInfo{},
+	ResourceInfo{ResourceUsage{&sync.RWMutex{}, 0, 0}, ResourceUsage{&sync.RWMutex{}, 0, 0}},
+	newValidator(),
+}
+
+type keyValue struct {
+	key   string
+	value string
 }
 
 // QueryString ... QueryString Values
 type QueryString struct {
-	Sleep      string `json:"sleep,omitempty"`
-	Size       string `json:"size,omitempty"`
-	Status     string `json:"status,omitempty"`
-	IfHost     string `json:"ifhost,omitempty"`
-	IfAZ       string `json:"ifaz,omitempty"`
-	IfServerIP string `json:"ifserverip,omitempty"`
-	IfTargetIP string `json:"iftargetip,omitempty"`
-	IfProxy1IP string `json:"ifproxy1ip,omitempty"`
-	IfProxy2IP string `json:"ifproxy2ip,omitempty"`
-	IfClientIP string `json:"ifclientip,omitempty"`
+	CPU          string `json:"cpu,omitempty"`
+	Memory       string `json:"memory,omitempty"`
+	Sleep        string `json:"sleep,omitempty"`
+	Size         string `json:"size,omitempty"`
+	Status       string `json:"status,omitempty"`
+	existsAction bool
+	IfClientIP   string `json:"ifclientip,omitempty"`
+	IfProxy1IP   string `json:"ifproxy1ip,omitempty"`
+	IfProxy2IP   string `json:"ifproxy2ip,omitempty"`
+	IfTargetIP   string `json:"iftargetip,omitempty"`
+	IfHostIP     string `json:"ifhostip,omitempty"`
+	IfHost       string `json:"ifhost,omitempty"`
+	IfAZ         string `json:"ifaz,omitempty"`
 }
 
-//  re := regexp.MustCompile("^[0-9]+([-,]?[0-9]+)*$")
-//  fmt.Println(re.FindString(",3103"))
-
-var store = &DataStore{&sync.RWMutex{}, ServerInfo{}, newValidator()}
+func (qs *QueryString) setValue(key, value string) {
+	switch key {
+	case "cpu":
+		qs.CPU = value
+	case "memory":
+		qs.Memory = value
+	case "sleep":
+		qs.Sleep = value
+	case "size":
+		qs.Size = value
+	case "status":
+		qs.Status = value
+	case "ifclientip":
+		qs.IfClientIP = value
+	case "ifproxy1ip":
+		qs.IfProxy1IP = value
+	case "ifproxy2ip":
+		qs.IfProxy2IP = value
+	case "iftargetip":
+		qs.IfTargetIP = value
+	case "ifhostip":
+		qs.IfHostIP = value
+	case "ifhost":
+		qs.IfHost = value
+	case "ifaz":
+		qs.IfAZ = value
+	}
+}
 
 func newValidator() map[string]*regexp.Regexp {
 	const (
-		regexpHostname = "^([a-zA-Z0-9-.]+)$"
-		regexpStatus   = "^(200|400|403|404|500|502|503|504)$"
+		regexpPercent  = "^(100|[0-9]{1,2})$"
 		regexpNumRange = "^([0-9]+)(?:-([0-9]+))?$"
-		regexpNumComma = "^([0-9]+)(?:,([0-9]+))*$" // 2個以上はFindStringSubmatchで取得不可のためmatchしたらstrings.Split
+		//regexpNumComma = "^([0-9]+)(?:,([0-9]+))*$" // 2個以上はFindStringSubmatchで取得不可のためmatchしたらstrings.Split
+		regexpStatus   = "^(200|400|403|404|500|502|503|504)$"
+		regexpHostname = "^([a-zA-Z0-9-.]+)$"
 		regexpAZone    = "^([a-z]{2}-[a-z]+-[1-9][a-d])$"
-		regexpIPv6     = "^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$"
 		regexpIPv4     = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+		regexpIPv6     = "^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$"
 	)
 	validator := map[string]*regexp.Regexp{}
+	validator["cpu"] = regexp.MustCompile(regexpPercent)
+	validator["memory"] = regexp.MustCompile(regexpPercent)
 	validator["sleep"] = regexp.MustCompile(regexpNumRange)
 	validator["size"] = regexp.MustCompile(regexpNumRange)
 	validator["status"] = regexp.MustCompile(regexpStatus)
 	validator["ifhost"] = regexp.MustCompile(regexpHostname)
 	validator["ifaz"] = regexp.MustCompile(regexpAZone)
-	validator["ifserverip"] = regexp.MustCompile(fmt.Sprintf("(%s|%s)", regexpIPv4, regexpIPv6))
+	validator["ifhostip"] = regexp.MustCompile(fmt.Sprintf("(%s|%s)", regexpIPv4, regexpIPv6))
 	validator["iftargetip"] = regexp.MustCompile(fmt.Sprintf("(%s|%s)", regexpIPv4, regexpIPv6))
 	validator["ifproxy1ip"] = regexp.MustCompile(fmt.Sprintf("(%s|%s)", regexpIPv4, regexpIPv6))
 	validator["ifproxy2ip"] = regexp.MustCompile(fmt.Sprintf("(%s|%s)", regexpIPv4, regexpIPv6))
@@ -88,59 +179,55 @@ func getIPAddress() string {
 }
 
 func main() {
-	store.Lock()
-	store.server.Name, _ = os.Hostname()
-	store.server.IP = getIPAddress()
-	store.Unlock()
+	fmt.Printf("%v\n", store)
+	store.host.Name, _ = os.Hostname()
+	store.host.IP = getIPAddress()
 	http.HandleFunc("/", handler)
 	srv := &http.Server{Addr: ":9000"}
 	log.Fatalln(srv.ListenAndServe())
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hostname: %s\nserverip: %s\n\n", store.server.Name, store.server.IP)
-	fmt.Fprintf(w, "path: %s\nquerystring: %s\n", r.URL.EscapedPath(), r.URL.Query().Encode())
-
-	fmt.Fprintf(w, "[QueryString]\n")
-	for _, kv := range sortkeyValues(r.URL.Query()) {
-		fmt.Fprintf(w, "  %s = %s\n", kv.key, kv.value)
+	reqInfo := RequestInfo{
+		Path:    r.URL.EscapedPath(),
+		Query:   r.URL.Query().Encode(),
+		Headers: combineValues(r.Header),
+	}
+	respInfo := ResponseInfo{
+		Host: store.host,
+		Resource: ResourceInfo{
+			CPU:    ResourceUsage{Target: store.resource.CPU.getTarget(), Current: store.resource.CPU.getCurrent()},
+			Memory: ResourceUsage{Target: store.resource.Memory.getTarget(), Current: store.resource.Memory.getCurrent()},
+		},
+		Request: reqInfo,
 	}
 
 	qs := validateQueryString(r.URL.Query())
-	s, _ := json.Marshal(qs)
+	respInfo.Request.Rawquery = qs
+	s, _ := json.MarshalIndent(respInfo, "", "  ")
 	fmt.Fprintf(w, "\n%s\n", string(s))
 }
 
-type keyValue struct {
-	key   string
-	value string
-}
-
-func sortkeyValues(input map[string][]string) (output []keyValue) {
-	keys := make([]string, 0, len(input))
-	for k := range input {
-		keys = append(keys, k)
+func combineValues(input map[string][]string) map[string]string {
+	output := map[string]string{}
+	for key := range input {
+		output[key] = strings.Join(input[key], ", ")
 	}
-	sort.Strings(keys)
-	for _, key := range keys {
-		if len(keys) > 1 {
-			sort.Strings(input[key])
-		}
-		for _, val := range input[key] {
-			output = append(output, keyValue{key: key, value: val})
-		}
-	}
-	return
+	return output
 }
 
 func validateQueryString(inQs map[string][]string) *QueryString {
 	qs := &QueryString{}
-	for _, kv := range sortkeyValues(inQs) {
-		if re, ok := store.validator[kv.key]; ok {
-			if len(re.FindStringSubmatch(kv.value)) > 0 {
-				fmt.Printf("  valid %s = %s\n", kv.key, kv.value)
+	for key, value := range combineValues(inQs) {
+		if re, ok := store.validator[key]; ok {
+			if len(re.FindStringSubmatch(value)) > 0 {
+				qs.setValue(key, value)
+				if !strings.HasPrefix(key, "if") {
+					qs.existsAction = true
+				}
+				fmt.Printf("  valid %s = %s\n", key, value)
 			} else {
-				fmt.Printf("invalid %s = %s\n", kv.key, kv.value)
+				fmt.Printf("invalid %s = %s\n", key, value)
 			}
 		}
 	}
